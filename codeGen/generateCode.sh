770f3d072734a7416a058683f2339ce52a41af47
rendong <leorendong@hotmail.com>
2016-07-30 18:37:09 +0800

    [dev] rename

--- codeGen/generateCode.sh
+++ codeGen/generateCode.sh
@@ -1,4 +1,4 @@
-java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkJavaScriptGenerator url http://115.28.160.84/info.api net.pocrd.m ../language/javascript/api
-java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkJavaGenerator url http://115.28.160.84/info.api net.pocrd.m.app.client ../language/java/apisdk/src/main/java/net/pocrd/m/app/client
-java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkObjectiveCGenerator url http://115.28.160.84/info.api POC ../language/objective-c/app/client/POCAPICore
-java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.HtmlApiDocGenerator url http://115.28.160.84/info.api ../language/htmldoc
+java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkJavaScriptGenerator url http://115.28.160.84/info.api net.pocrd.m ../sdk-language/javascript/api
+java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkJavaGenerator url http://115.28.160.84/info.api net.pocrd.m.app.client ../sdk-language/java/apisdk/src/main/java/net/pocrd/m/app/client
+java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.ApiSdkObjectiveCGenerator url http://115.28.160.84/info.api POC ../sdk-language/objective-c/app/client/POCAPICore
+java -classpath api-util-1.2.2-SNAPSHOT-allinone.jar net.pocrd.core.generator.HtmlApiDocGenerator url http://115.28.160.84/info.api ../sdk-language/htmldoc
