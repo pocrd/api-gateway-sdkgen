929fb1784ce8ad370e76595e6cdf0a885a9bf264
rendong <leorendong@hotmail.com>
2017-07-27 11:54:27 +0800

    [dev] add interface

---
+++ sdk-language/java/apisdk/src/main/java/net/pocrd/m/app/client/util/JsonSerializable.java
@@ -0,0 +1,10 @@
+package net.pocrd.m.app.client.util;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Created by rendong on 2017/7/26.
+ */
+public interface JsonSerializable {
+    JsonObject serialize();
+}
