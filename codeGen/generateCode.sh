73b3b73e282932b92e4155f5771a8d3d177be48f
rendong <leorendong@hotmail.com>
2017-08-04 15:52:58 +0800

    [dev] update code gen jar

--- codeGen/generateCode.sh
+++ codeGen/generateCode.sh
@@ -12,7 +12,7 @@ mkdir ../sdk-language/objective-c/apisdk/apisdk/PoCAPICore/Request
 mkdir ../sdk-language/objective-c/apisdk/apisdk/PoCAPICore/Response
 
 
-java -jar apigen.jar api-jssdk-gen -url http://api-stage.yit.com/apigw/info.api?raw -o ../sdk-language/javascript/api net.pocrd.m
-java -jar apigen.jar api-javasdk-gen -url http://api-stage.yit.com/apigw/info.api?raw -o ../sdk-language/java/apisdk/src/main/java/net/pocrd/m/app/client net.pocrd.m.app.client
-java -jar apigen.jar api-oc-gen -url http://api-stage.yit.com/apigw/info.api?raw -o ../sdk-language/objective-c/apisdk/apisdk/PoCAPICore PoC
-java -jar apigen.jar api-doc-gen -url http://api-stage.yit.com/apigw/info.api?raw -o ../sdk-language/htmldoc
+java -jar apigen.jar api-jssdk-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/javascript/api net.pocrd.m
+java -jar apigen.jar api-javasdk-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/java/apisdk/src/main/java/net/pocrd/m/app/client net.pocrd.m.app.client
+java -jar apigen.jar api-oc-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/objective-c/apisdk/apisdk/PoCAPICore PoC
+java -jar apigen.jar api-doc-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/htmldoc
