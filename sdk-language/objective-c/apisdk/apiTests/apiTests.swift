286830b17e2f234d6447491bf6fdbc2c6347572f
rendong <leorendong@hotmail.com>
2017-08-04 15:41:29 +0800

    [dev] add test case

---
+++ sdk-language/objective-c/apisdk/apiTests/apiTests.swift
@@ -0,0 +1,41 @@
+//
+//  apiTests.swift
+//  apiTests
+//
+//  Created by rendong on 2017/8/3.
+//  Copyright © 2017年 pocrd.net All rights reserved.
+//
+
+import XCTest
+
+class apiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        var context = PoCApiContext(appid: "1", vercode: 1, vername: "1.0");
+        var accessor = PoCApiAccessor(apiContext: context, connTimeout: 3000, soTimeout: 1000, agent: "swift test", keepalive: 61000, apiUrl: "http://www.pocrd.net/m.api");
+        accessor!.referer = "123";
+        
+        print("hello world." + accessor!.referer);
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ sdk-language/objective-c/apisdk/apisdk/apisdk-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "PoCApiAccessor.h"
+#import "PoCApiContext.h"
+#import "PoCApiRequestInclude.h"
+#import "PoCApiResponseInclude.h"
