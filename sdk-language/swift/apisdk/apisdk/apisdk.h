25ebfc918ac3cc66b20872e499913c95dd18463f
rendong <leorendong@hotmail.com>
2017-08-14 10:25:50 +0800

    [dev] init swift apisdk

---
+++ sdk-language/swift/apisdk/apisdk/apisdk.h
@@ -0,0 +1,19 @@
+//
+//  apisdk.h
+//  apisdk
+//
+//  Created by rendong on 2017/8/14.
+//  Copyright © 2017年 net.pocrd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for apisdk.
+FOUNDATION_EXPORT double apisdkVersionNumber;
+
+//! Project version string for apisdk.
+FOUNDATION_EXPORT const unsigned char apisdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <apisdk/PublicHeader.h>
+
+
