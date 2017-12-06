839f26d13b80d1638e328951ee9aead8cebdde12
rendong <leorendong@hotmail.com>
2017-12-06 15:50:34 +0800

    [dev] fix

--- codeGen/generateCode.sh
+++ codeGen/generateCode.sh
@@ -15,7 +15,7 @@ mkdir ../sdk-language/csharp/apisdk/PoCRD.Client/PoCRD.Client/Client/API
 
 java -jar apigen.jar api-jssdk-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/javascript/api net.pocrd.m
 java -jar apigen.jar api-javasdk-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/java/apisdk/src/main/java/net/pocrd/m/app/client net.pocrd.m.app.client
-java -jar apigen.jar api-csharp-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/csharp/apisdk/PoCRD.Client/PoCRD.Client/Client PoCRD
+java -jar apigen.jar api-csharp-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/csharp/apisdk/PoCRD.Client/PoCRD/Client PoCRD
 java -jar apigen.jar api-oc-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/objective-c/apisdk/apisdk/PoCAPICore PoC
 java -jar apigen.jar api-doc-gen -url http://www.pocrd.net/info.api?raw -o ../sdk-language/htmldoc
 
